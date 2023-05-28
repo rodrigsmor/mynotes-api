506f47edc757d2b0fbb458a097dcc6fcb78fbfd4
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-05-28 12:56:04 -0300

    :test_tube: test: Get user overview test

--- src/main/java/com/rm/mynotes/service/impl/UserServiceImplementation.java
+++ src/main/java/com/rm/mynotes/service/impl/UserServiceImplementation.java
@@ -12,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
-import java.time.Month;
 import java.time.OffsetDateTime;
 
 @Slf4j
